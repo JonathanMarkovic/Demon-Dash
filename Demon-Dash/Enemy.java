cb700cd75cfb4148b219bf2d7ac5268156de0364
JonathanMarkovic <jonathan.markovic@outlook.com>
2024-05-09 16:15:22 -0400

    Enemy movement implemented
    enemies will follow the player based on X coordinate speed can be adjusted with enemySpeed if needed

--- Demon-Dash/Enemy.java
+++ Demon-Dash/Enemy.java
@@ -6,18 +6,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Enemy extends SimulationActor
+public class Enemy extends Player
 {
     private int health = 3;
     private int score = 10;
+    private int enemySpeed = 2;
+    
+    public Enemy() {
+        super(); 
+        this.acceleration = new Vector2D(0.0,-10.0);
+    }
+    
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        takeDamage();
+        eulerIntegration();
         
+        checkCollision();
+        takeDamage(); 
+        if (health != 0) {
+            move();
+        }
     }
     
     public void takeDamage() {
@@ -32,6 +44,31 @@ public class Enemy extends SimulationActor
     }
     
     public void move() {
+        PlayerShooter player = getWorld().getObjects(PlayerShooter.class).get(0);
+        int x = player.getX();
+        int y = player.getY();
+        int movement = 0;
         
+        if (getX() - x < 0) {
+            movement = -1;
+        } else if (getX() - x > 0) {
+            movement = 1;
+        } else if (getX() - x == 0) {
+            movement = 0;
+        }
+        
+        if (movement < 0) 
+       { 
+            this.velocity = new Vector2D(enemySpeed, this.velocity.getY());   
+       }
+       
+       else if (movement > 0) 
+       { 
+            this.velocity = new Vector2D(-enemySpeed, this.velocity.getY());      
+       }
+       else if (movement == 0)
+       {
+           this.velocity = new Vector2D(0, this.velocity.getY());  
+       }
     }
 }
